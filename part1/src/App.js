583e035e1d0124a07d4db79bb9f9f2cf1df04dc2
luisina.muller <luisina.e.muller@gmail.com>
2024-01-21 10:35:15 -0300

    firts commit

---
+++ part1/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Mensaje from './mensaje';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+     <Mensaje />
+    </div>
+  );
+}
+
+export default App;
